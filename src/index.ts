95f34d8c48e22f52acbfb66985a87d36b3578e4b
Andrii Kolisnyk <scear.sic@gmail.com>
2020-07-20 14:32:15 +0200

    Added method for calculation standard deviation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export const deviation = (name: string): string => {
-    return name;
+const arithmeticMean = (arr: number[]): number => {
+    return arr.reduce((previousValue, currentValue) => {
+        return previousValue + currentValue;
+    }, 0);
 }
+
+const Pow = (arr: number[], mean: number): number => {
+    return arr.reduce((previousValue, currentValue) => {
+        return previousValue + Math.pow(currentValue - mean, 2);
+    }, 0);
+}
+
+export const deviation = (arr: number[]): number => {
+    const mean = arithmeticMean(arr) / arr.length;
+    return Math.sqrt(Pow(arr, mean) / arr.length);
+}
+
+const Arr = [172.3, 170.4, 169.4, 169, 167, 173];
+console.log(deviation(Arr));
