5a5174ad8ad4b559729229da7e5fa03c168df031
Andrii Kolisnyk <scear.sic@gmail.com>
2020-09-02 22:00:54 +0200

    #1: Fix issue with running tests on Mac os

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,4 +4,4 @@ test("Should calculate standard deviations", () => {
     const Arr = [172.3, 170.4, 169.4, 169, 167, 173];
     const expected = deviation(Arr)
     expect(expected).toBe(2.0251886057572257);
-})
+});
