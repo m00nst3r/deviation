0bd480e8d9dd14e540a399833a886de7d8380109
Andrii Kolisnyk <scear.sic@gmail.com>
2020-09-07 19:03:39 +0200

    #4: Add unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,17 @@
-import sd from "./index"
+import sd, { arithmeticMean, Pow } from "./index"
 
 test("Should calculate standard deviations", () => {
     const Arr = [172.3, 170.4, 169.4, 169, 167, 173];
     const expected = sd(Arr);
     expect(expected).toBe(2.0251886057572257);
 });
+
+test("Get arithmetic mean", () => {
+  const mean = arithmeticMean([1, 2, 4, 6]);
+  expect(mean).toBe(3.25);
+})
+
+test("", () => {
+  const expected = Pow([5, 4], 2);
+  expect(expected).toBe(13);
+})
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-const arithmeticMean = (arr: number[]): number => {
-    return arr.reduce((previousValue, currentValue) => {
+export const arithmeticMean = (arr: number[]): number => {
+    const mean = arr.reduce((previousValue, currentValue) => {
         return previousValue + currentValue;
     }, 0);
+
+    return mean / arr.length;
 }
 
-const Pow = (arr: number[], mean: number): number => {
+export const Pow = (arr: number[], mean: number): number => {
     return arr.reduce((previousValue, currentValue) => {
         return previousValue + Math.pow(currentValue - mean, 2);
     }, 0);
 }
 
-export const deviation = (arr: number[]): number => {
-    const mean = arithmeticMean(arr) / arr.length;
+const deviation = (arr: number[]): number => {
+    const mean = arithmeticMean(arr);
     return Math.sqrt(Pow(arr, mean) / arr.length);
 }
 
